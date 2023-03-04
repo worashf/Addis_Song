d8e4b7207943821b0b6af67cbbbee9e4951be719
worashf <uppert83@gmail.com>
2023-03-04 23:07:36 +0300

    Prepare for frontend deployment

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios  from  "axios"
-const baseURL = `http://localhost:5000/api/v1`
+const baseURL = `https://addissong.onrender.com/api/v1`
 const axiosInstance = axios.create({
     baseURL,
 })
