ebe3e0e63d4b36cf427f53111a64bc4dbbd45e90
worashf <uppert83@gmail.com>
2023-03-05 00:05:55 +0300

    Change backend deployemnt

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios  from  "axios"
-const baseURL = `http://localhost:5000/api/v1`
+const baseURL = `https://addismus.onrender.com/api/v1`
 const axiosInstance = axios.create({
     baseURL,
 })
